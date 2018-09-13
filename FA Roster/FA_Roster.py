51565d59feb9074e1672fcdaa735da84d20ca96d
Jonathan Nguyen <jon.p.nguyen@pm.me>
2018-09-13 15:19:02 -0500

    Added comments to code

--- FA Roster/FA_Roster.py
+++ FA Roster/FA_Roster.py
@@ -7,16 +7,15 @@ import os
 os.chdir("/Volumes/Groups/Enrollment Management Center/FA Roster")
 
 file = input('Drop the file you want here:\n')
-timr = time.strftime("%m-%d-%y")
+timr = time.strftime("%m-%d-%y")  # Sets the file name to current date
 bar = IncrementalBar(max=15)
 
 try:
     for i in range(15):
         data = pd.read_csv(file, sep='|', header=0, skiprows=5)
-        data.drop(data.columns[0], axis=1, inplace=True)
-        data.drop('EMAIL', axis=1, inplace=True)
-        data.drop(data.index[200:], inplace=True)
-        data.sort_values(['REGTERM', 'PELL', 'APPTERM'],
+        data.drop('EMAIL', axis=1, inplace=True)  # Deletes Email column
+        data.drop(data.index[200:], inplace=True)  # Limits rows to 200
+        data.sort_values(['REGTERM', 'PELL', 'APPTERM'],  # Sort order
                          ascending=[False, False, False], inplace=True)
 
         data.to_excel(timr + '.xlsx', index=False)
@@ -26,5 +25,6 @@ try:
     bar.finish()
     print('\nComplete!!!!')
 
+# If user didn't remove quotes or spaces
 except FileNotFoundError:
     print('\nMake sure there are no spaces or quotes and try again\n')
