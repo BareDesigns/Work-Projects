d31f27e624e4078ceef50cdba334d63ab654e962
Jonathan Nguyen <jon.p.nguyen@pm.me>
2018-09-13 15:10:16 -0500

    Clean Upload

--- FA Roster/FA_Roster.py
+++ FA Roster/FA_Roster.py
@@ -1,19 +1,17 @@
 import pandas as pd
-# from progress.bar import IncrementalBar
+from progress.bar import IncrementalBar
 import time
 import os
 
-os.chdir("G:\Enrollment Management Center\FA Roster")
-# os.chdir("/Volumes/Groups/Enrollment Management Center/FA Roster")
+# os.chdir("G:\Enrollment Management Center\FA Roster")
+os.chdir("/Volumes/Groups/Enrollment Management Center/FA Roster")
 
-file = input('Drop the file you want here\n')
+file = input('Drop the file you want here:\n')
 timr = time.strftime("%m-%d-%y")
-# bar = MoonSpinner('\nProcessing: ')
-# bar = IncrementalBar(max=15)
+bar = IncrementalBar(max=15)
 
 try:
     for i in range(15):
-        # Do some work
         data = pd.read_csv(file, sep='|', header=0, skiprows=5)
         data.drop(data.columns[0], axis=1, inplace=True)
         data.drop('EMAIL', axis=1, inplace=True)
@@ -21,14 +19,12 @@ try:
         data.sort_values(['REGTERM', 'PELL', 'APPTERM'],
                          ascending=[False, False, False], inplace=True)
 
-        # data.to_csv(timr + '.csv')
         data.to_excel(timr + '.xlsx', index=False)
 
         time.sleep(0.2)
-        # bar.next()
-    # bar.finish()
+        bar.next()
+    bar.finish()
     print('\nComplete!!!!')
 
-# except OSError:
 except FileNotFoundError:
     print('\nMake sure there are no spaces or quotes and try again\n')
